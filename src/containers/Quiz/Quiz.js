cacb503d8906107295e79ba37f128bc169911d17
Serg Ch <chivirdos@gmail.com>
2020-01-24 23:32:56 +0300

    second commit

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react"
+import classes from './Quiz.module.css'
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div className={classes.Quiz}>
+
+            </div>
+        )
+    }
+}
+
+export default Quiz
