c9b20faec7be24964644ad27ac336ac566988ae5
Sergey Chivirda <si.chivirda@gmail.com>
2020-01-25 15:10:54 +0300

    49 урок готов

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -3,29 +3,39 @@ import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
-    state = {
-        quiz: [
-            {
-                answers: [
-                    {text: 'Вопрос 1'},
-                    {text: 'Вопрос 2'},
-                    {text: 'Вопрос 3'},
-                    {text: 'Вопрос 4'}
-                ]
-            }
+  state = {
+    quiz: [
+      {
+        question: 'Какого цвета небо?',
+        rightAnswerId: 2,
+        answers: [
+          {text: 'Чёрный', id: 1},
+          {text: 'Синий', id: 2},
+          {text: 'Красный', id: 3},
+          {text: 'Зелёный', id: 4}
         ]
-    }
-    
-    render() {
-        return (
-            <div className={classes.Quiz}>
-                <div className={classes.QuizWrapper}>
-                    <h1>Ответьте на все вопросы</h1>
-                    <ActiveQuiz answers={this.state.quiz[0].answers} />
-                </div>
-            </div>
-        )
-    }
+      }
+    ]
+  }
+
+  onAnswerClickHandler = answerId => {
+    console.log('Answer Id: ', answerId)
+  }
+
+  render() {
+    return (
+      <div className={classes.Quiz}>
+        <div className={classes.QuizWrapper}>
+          <h1>Ответьте на все вопросы</h1>
+          <ActiveQuiz
+            answers={this.state.quiz[0].answers}
+            question={this.state.quiz[0].question}
+            onAnswerClick={this.onAnswerClickHandler}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Quiz
