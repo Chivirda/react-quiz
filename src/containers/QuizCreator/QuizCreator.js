9e61386510c0478118737fc90f7e09001fb15df8
Serg Ch <chivirdos@gmail.com>
2020-01-29 21:56:06 +0300

    82 урок готов

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -3,7 +3,7 @@ import classes from './QuizCreator.module.css'
 import Button from "../../components/UI/Button/Button"
 import Input from "../../components/UI/Input/Input";
 import Select from "../../components/UI/Select/Select";
-import {createControl} from '../../form/formFramework'
+import {createControl, validate, validateForm} from '../../form/formFramework'
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
 
 const createOptionControl = number => {
@@ -31,6 +31,7 @@ class QuizCreator extends Component {
 
   state = {
     quiz: [],
+    isFormValid: false,
     rightAnswerId: 1,
     formControls: createFormControls()
   }
@@ -39,16 +40,28 @@ class QuizCreator extends Component {
     event.preventDefault()
   }
 
-  addQuestionHandler = () => {
-
+  addQuestionHandler = event => {
+    event.preventDefault()
   }
 
   createQuizHandler = () => {
 
   }
 
-  chahgeHandler = (value, controlName) => {
+  changeHandler = (value, controlName) => {
+    const formControls = { ...this.state.formControls }
+    const control = { ...formControls[controlName] }
 
+    control.touched = true
+    control.value = value
+    control.valid = validate(control.value, control.validation)
+
+    formControls[controlName] = control
+
+    this.setState({
+      formControls,
+      isFormValid: validateForm(formControls)
+    })
   }
 
   renderControls() {
@@ -60,10 +73,10 @@ class QuizCreator extends Component {
             label={control.label}
             value={control.value}
             valid={control.valid}
-            shouldValidete={!!control.validation}
+            shouldValidate={!!control.validation}
             touched={control.touched}
             errorMessage={control.errorMessage}
-            onChange={event => this.chahgeHandler(event.target.value, controlName)}
+            onChange={event => this.changeHandler(event.target.value, controlName)}
           />
           {index === 0 ? <hr/> : null}
         </Auxiliary>
@@ -104,6 +117,7 @@ class QuizCreator extends Component {
             <Button
               type="primary"
               onClick={this.addQuestionHandler}
+              disabled={!this.state.isFormValid}
             >
               Добавить вопрос
             </Button>
@@ -111,6 +125,7 @@ class QuizCreator extends Component {
             <Button
               type="success"
               onClick={this.createQuizHandler}
+              disabled={this.state.quiz.length === 0}
             >
               Создать тест
             </Button>
